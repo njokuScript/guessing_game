913b5cdc5be1d25f1b44421241413a753cbe15cc
“Njoku <kalunjoku123@gmail.com>
2021-01-02 12:11:38 +0100

    random number gen

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+use rand::Rng;
+
+fn main() {
+    println!("guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1,101);
+
+    println!("The secret number is : {}", secret_number);
+
+    println!("Please input your guess");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+
+    println!("you guessed : {}", guess);
+}
